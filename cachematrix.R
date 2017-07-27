6da3a87fe45b9dade315c2923278b688807de8e7
gralspau <gralspaugh@davidson.edu>
2017-07-27 13:59:05 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -6,19 +6,23 @@
 ##Creating a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        #Setting value of the matrix
+        #Setting value of the matrix object
         inv <- NULL
-        set <- function(y = as.matrix()) {
+        set <- function(y = matrix()) {
                 x <<- y
                 inv <<- NULL
         }
         #Getting value of the matrix
+        
         get <- function() x
+        
         #Setting value of matrix inverse
         setinverse <- function(solve) inv <<- solve
+        
         #Getting value of matrix inverse
         getinverse <- function() inv
-        #Creation of special matrix containing function to cache inverse
+        
+        #List of functions to be drawn from in cacheSolve
         list(set = set, get = get, setinverse = setinverse, 
              getinverse = getinverse)
 }
@@ -31,13 +35,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inv <- x$getinverse()
-        if(!is.null(inv)) {
+        inv <- x$getinverse() #retrieves inverse matrix value
+        if(!is.null(inv)) {  #fetches matrix from cache if already calculated
                 message("getting cached data")
                 return(inv)
         }
-        data <- x$get()
-        inv <- solve(data)
-        x$setinverse(inv)
+        data <- x$get() #fetches matrix if conditional not met
+        inv <- solve(data, ...) #calculates inverse
+        x$setinverse(inv) #sets inverse
         inv
 }
+
+##returns 4x4 matrix when x <- matrix(sample(16), 4, 4)*matrix(sample(16), 4, 4)
